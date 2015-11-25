1545f6cb0bb7396ee24d6296917d1f9bae8f5b2e
Aleixo Sánchez <aleixosk@gmail.com>
2015-11-25 10:47:08 +0000

    buttons function

--- app/src/main/java/mosaic/happin/Login.java
+++ app/src/main/java/mosaic/happin/Login.java
@@ -2,6 +2,8 @@ package mosaic.happin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void login(View view) {
+        TextView email = (TextView) findViewById(R.id.email);
+    }
+
+    public void signUp(View view) {
+
+    }
+
+    public void forgotPwd(View view) {
+
+    }
+
 }
