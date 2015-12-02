6a6d3b2a6089b35cd81eada8a2f3ebb907b4c8db
Carlos <19charles96@gmail.com>
2015-12-02 14:22:21 +0000

    tabs working with the map
    onDetach and OnDestroyView implemented

--- app/src/main/java/mosaic/happin/MapsActivity.java
+++ app/src/main/java/mosaic/happin/MapsActivity.java
@@ -1,10 +1,14 @@
 package mosaic.happin;
 
+import android.app.Activity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,6 +20,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
 
     public MapsActivity(){
 
@@ -25,15 +30,21 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_maps, container, false);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        FragmentManager fm = getChildFragmentManager();
+        mapFragment  = (SupportMapFragment) fm.findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         return view;
     }
-
 
     /**
      * Manipulates the map once available.
@@ -48,9 +59,29 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Bristol and move the camera
         LatLng Bristol = new LatLng(51.465411, -2.585911);
         mMap.addMarker(new MarkerOptions().position(Bristol).title("Bristol"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Bristol, 13));
+        mMap.setMyLocationEnabled(true);
     }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        killOldMap();
+    }
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        killOldMap();
+    }
+    private void killOldMap() {
+
+        if(mapFragment != null) {
+            FragmentManager fM = getFragmentManager();
+            fM.beginTransaction().remove(mapFragment).commit();
+        }
+    }
+
 }
