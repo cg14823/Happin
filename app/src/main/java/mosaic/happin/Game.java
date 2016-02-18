1ec2df4a2d91a031a0c5e721433f7fe5fac38cd6
TomWPJ <tj14830@my.bristol.ac.uk>
2016-02-18 18:18:55 +0000

    Camera Position locked
    Need to update position with current location dynamically

--- app/src/main/java/mosaic/happin/Game.java
+++ app/src/main/java/mosaic/happin/Game.java
@@ -1,11 +1,39 @@
 package mosaic.happin;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 
 /**
@@ -18,12 +46,148 @@ public class Game extends Fragment {
         // Required empty public constructor
     }
 
+    private GoogleMap mMap;
+    private MapView mapView;
+    Firebase ref;
+    ArrayList<Place> places;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        Firebase.setAndroidContext(getContext());
+        ref = new Firebase("https://flickering-torch-2192.firebaseio.com/places");
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) view.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mMap = mapView.getMap();
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+        mMap.setMyLocationEnabled(true);
+
+
+
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        try {
+            MapsInitializer.initialize(this.getActivity());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        getPlaces();
+
+        /* setLocationCheck creates a location button listener, if someone clicks it will check if
+        * the location service is enabled, if it is not it ask you if you want to activate it
+        * otherwise it will just zoom to your current location (High-precision not best choice?)*/
+        setLocationCheck();
+
+        return view;
+    }
+
+    private void setLocationCheck() {
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                // Acquire a reference to the system Location Manager
+                LocationManager manager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+                if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    // If Location disable create a alert dialog
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+                    alertDialogBuilder.setMessage("Location is disabled in your device. Would you like to enable it?")
+                            // Have to respond to this message not cancelable
+                            .setCancelable(false)
+                                    // If yes open setting page to enable location
+                            .setPositiveButton("Yes",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            // intent calls the android activity of location settings
+                                            Intent callGPSSettingIntent = new Intent(
+                                                    android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                            startActivity(callGPSSettingIntent);
+                                        }
+                                    });
+                    //if no close the dialog
+                    alertDialogBuilder.setNegativeButton("Maybe Later",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert = alertDialogBuilder.create();
+                    alert.show();
+                }
+
+                return false;
+            }
+        });
+    }
+
+
+    /* This function should ge the top rated places from the server*/
+    private void getPlaces(){
+        places = new ArrayList<Place>();
+        ref = new Firebase("https://flickering-torch-2192.firebaseio.com/places");
+        Query likeQuery = ref.orderByChild("likes").limitToLast(10);
+        likeQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot querySnapshot) {
+                for (DataSnapshot d : querySnapshot.getChildren()) {
+                    ref.child(d.getKey()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                Place p = dataSnapshot.getValue(Place.class);
+                                mMap.addMarker(new MarkerOptions().position(new LatLng(p.getLat(), p.getLon()))
+                                        .title(p.getName()).snippet(p.getDescription()));
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(FirebaseError firebaseError) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+                showToast(error.getMessage());
+            }
+        });
+
+        LatLng bristol = new LatLng(51.465411, -2.585911);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bristol, 17));
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    private void showToast(String message){
+        Toast toast = Toast.makeText(getContext(),
+                message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
 }
